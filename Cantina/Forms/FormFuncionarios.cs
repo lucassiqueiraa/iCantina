bea6195a7a0c57b80726f9d91f04fbf032bf1e9c
Lucas Siqueira <lucassiqueira0763@hotmail.com>
2024-06-18 15:35:35 +0100

    Revert "merge"
    This reverts commit a635587ec4be36dc42b27d202d87760b691b8d08.

---
+++ Cantina/Forms/FormFuncionarios.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina.Forms
+{
+    public partial class FormFuncionarios : Form
+    {
+        public FormFuncionarios()
+        {
+            InitializeComponent();
+        }
+        private void btnVoltarMenuPrincipal_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show(this);
+            this.Hide();
+        }
+
+        private void btnListarFuncionarios_Click(object sender, EventArgs e)
+        {
+            ListarFuncionarios();
+        }
+
+        private void ListarFuncionarios()
+        {
+
+        }
+
+        private  void btnGravarFuncionario_Click(object sender, EventArgs e)
+        {
+
+            ///<summary>
+            ///aqui vem a lógica de gravar na base de dados.
+            ///</summary>
+
+        }
+        
+       
+    }
+}
---
+++ Cantina/Forms/FormMultas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina.Forms
+{
+    public partial class FormMultas : Form
+    {
+        public FormMultas()
+        {
+            InitializeComponent();
+        }
+    }
+}

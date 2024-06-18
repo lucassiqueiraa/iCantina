065f6db1343410cff14cc34167e10d42c08914b0
Altamir Rodrigues <box.altamir@gmail.com>
2024-06-18 17:50:00 +0100

    Gravar Funcionario
    Btn Gravar Funcionario ok

--- Cantina/Forms/FormFuncionarios.cs
+++ Cantina/Forms/FormFuncionarios.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using Cantina.Data;
+using Cantina.Models;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Cantina.Forms
@@ -30,18 +27,87 @@ namespace Cantina.Forms
 
         private void ListarFuncionarios()
         {
+            List<Funcionario> funcionarios;
+            using (var context = new CantinaContext())
+            {
+                funcionarios = context.Funcionarios.ToList();
+            }
+            listBoxFuncionarios.Items.Clear();
+
+            // Usando HashSet para armazenar IDs únicos
+            HashSet<int> idsAdicionados = new HashSet<int>();
+            foreach (var funcionario in funcionarios)
+            {
+                //Verifica se o ID já foi adicionado a Lista
+
+                if(!idsAdicionados.Contains(funcionario.Id))
+                {
+
+                    listBoxFuncionarios.Items.Add($"ID: {funcionario.Id}, Nome: {funcionario.Nome}, NIF: {funcionario.NIF}, Username: {funcionario.Username}");
+                    //Aqui vai adicionar o id a lista de ids adicionados.
+                    idsAdicionados.Add(funcionario.Id);
+                }
+
+            }
+        }
+
+        private void btnGravarFuncionario_Click(object sender, EventArgs e)
+        {
+            ///<summary>
+            ///Captura de dados através dos formulários
+            ///</summary>
+            ///
+         
+            string nome = textNome.Text.Trim();
+            string nifText = textNif.Text.Trim();  
+            string username = textUsername.Text.Trim();
+
+            // Validação simples dos campos de entrada
+
+            if(string.IsNullOrEmpty(nome)||string.IsNullOrEmpty(nifText) || string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Por favor, preencha todos os dados.");
+                return;
+
+            }
+
+            //Aqui precisei alterar/converter o NIF para int
+            if (!int.TryParse(nifText, out int nif))
+            {
+                MessageBox.Show("O nif deve ser um número válido!!");
+            }
+
+            Funcionario funcionario = new Funcionario
+            {
+                Nome = nome,
+                NIF = nif,
+                Username = username,
+            };
+            //Aqui adicionamos o novo funcionario ao contexto e salvamos na base de dados   
+            using(var context = new CantinaContext())
+            {
+                context.Funcionarios.Add(funcionario);
+                context.SaveChanges();
+
+            }
+            //Aqui limpamos os campos após a gravação 
+            textNome.Text = "";
+            textNif.Text = "";
+            textUsername.Text = "";
+
+            //atualizando a lista de funcionarios 
+            ListarFuncionarios();
+
+
 
         }
 
-        private  void btnGravarFuncionario_Click(object sender, EventArgs e)
+        private void FormFuncionarios_Load(object sender, EventArgs e)
         {
 
-            ///<summary>
-            ///aqui vem a lógica de gravar na base de dados.
-            ///</summary>
-
         }
-        
+    }
+
        
     }
-}
+
--- Cantina/Program.cs
+++ Cantina/Program.cs
@@ -1,37 +1,37 @@
-﻿using Cantina.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
-using System.Data.Entity;
-using Cantina.Data;
+﻿    using Cantina.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+    using System.Data.Entity;
+    using Cantina.Data;
 
 
-namespace Cantina
-{
-    internal static class Program
+    namespace Cantina
     {
-       
-        [STAThread]
-        static void Main()
+        internal static class Program
         {
-            using (var db = new CantinaContext())
+       
+            [STAThread]
+            static void Main()
             {
-                var funcionario = new Funcionario { Id = 1, Nome = "Lucas Siqueira", NIF = 292692315, Username = "lucas" };
-                db.Funcionarios.Add(funcionario);
-                var prato = new Prato { Id = 1, Descricao = "Bacalhau á brás", Tipo = "Peixe", Ativo = true };
-                db.Pratos.Add(prato);
-                db.SaveChanges();
+                using (var db = new CantinaContext())
+                {
+                    var funcionario = new Funcionario { Id = 1, Nome = "Lucas Siqueira", NIF = 292692315, Username = "lucas" };
+                    db.Funcionarios.Add(funcionario);
+                    var prato = new Prato { Id = 1, Descricao = "Bacalhau á brás", Tipo = "Peixe", Ativo = true };
+                    db.Pratos.Add(prato);
+                    db.SaveChanges();
+                }
+
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+            
+
+            
             }
-
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            
-
-            
         }
     }
-}

71834167aad3bf6a894b571e2ba8eed52becd7d6
Altamir Rodrigues <box.altamir@gmail.com>
2024-06-19 22:44:54 +0100

    Reserva

--- Cantina/Forms/FormReservas.Designer.cs
+++ Cantina/Forms/FormReservas.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.labelNomeCliente = new System.Windows.Forms.Label();
-            this.labelSaldoCliente = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textNif = new System.Windows.Forms.TextBox();
             this.btnBuscarClientePorNif = new System.Windows.Forms.Button();
+            this.textNif = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelSaldoCliente = new System.Windows.Forms.Label();
+            this.labelNomeCliente = new System.Windows.Forms.Label();
             this.listBoxMenus = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnMenuPrincipal = new System.Windows.Forms.Button();
+            this.listBoxExtras = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,40 +54,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Busca Por Nif";
             // 
-            // labelNomeCliente
-            // 
-            this.labelNomeCliente.AutoSize = true;
-            this.labelNomeCliente.Location = new System.Drawing.Point(49, 141);
-            this.labelNomeCliente.Name = "labelNomeCliente";
-            this.labelNomeCliente.Size = new System.Drawing.Size(141, 25);
-            this.labelNomeCliente.TabIndex = 0;
-            this.labelNomeCliente.Text = "Nome Cliente";
-            // 
-            // labelSaldoCliente
-            // 
-            this.labelSaldoCliente.AutoSize = true;
-            this.labelSaldoCliente.Location = new System.Drawing.Point(263, 141);
-            this.labelSaldoCliente.Name = "labelSaldoCliente";
-            this.labelSaldoCliente.Size = new System.Drawing.Size(67, 25);
-            this.labelSaldoCliente.TabIndex = 1;
-            this.labelSaldoCliente.Text = "Saldo";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 53);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Nif : ";
-            // 
-            // textNif
-            // 
-            this.textNif.Location = new System.Drawing.Point(68, 47);
-            this.textNif.Name = "textNif";
-            this.textNif.Size = new System.Drawing.Size(189, 31);
-            this.textNif.TabIndex = 3;
-            // 
             // btnBuscarClientePorNif
             // 
             this.btnBuscarClientePorNif.Location = new System.Drawing.Point(309, 21);
@@ -97,6 +64,40 @@
             this.btnBuscarClientePorNif.UseVisualStyleBackColor = true;
             this.btnBuscarClientePorNif.Click += new System.EventHandler(this.btnBuscarClientePorNif_Click);
             // 
+            // textNif
+            // 
+            this.textNif.Location = new System.Drawing.Point(68, 47);
+            this.textNif.Name = "textNif";
+            this.textNif.Size = new System.Drawing.Size(189, 31);
+            this.textNif.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nif : ";
+            // 
+            // labelSaldoCliente
+            // 
+            this.labelSaldoCliente.AutoSize = true;
+            this.labelSaldoCliente.Location = new System.Drawing.Point(263, 141);
+            this.labelSaldoCliente.Name = "labelSaldoCliente";
+            this.labelSaldoCliente.Size = new System.Drawing.Size(67, 25);
+            this.labelSaldoCliente.TabIndex = 1;
+            this.labelSaldoCliente.Text = "Saldo";
+            // 
+            // labelNomeCliente
+            // 
+            this.labelNomeCliente.AutoSize = true;
+            this.labelNomeCliente.Location = new System.Drawing.Point(49, 141);
+            this.labelNomeCliente.Name = "labelNomeCliente";
+            this.labelNomeCliente.Size = new System.Drawing.Size(141, 25);
+            this.labelNomeCliente.TabIndex = 0;
+            this.labelNomeCliente.Text = "Nome Cliente";
+            // 
             // listBoxMenus
             // 
             this.listBoxMenus.FormattingEnabled = true;
@@ -106,22 +107,32 @@
             this.listBoxMenus.Size = new System.Drawing.Size(774, 154);
             this.listBoxMenus.TabIndex = 1;
             // 
-            // button1
+            // btnMenuPrincipal
             // 
-            this.button1.Location = new System.Drawing.Point(811, 632);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(191, 93);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Menu Principal";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnMenuPrincipal.Location = new System.Drawing.Point(877, 646);
+            this.btnMenuPrincipal.Name = "btnMenuPrincipal";
+            this.btnMenuPrincipal.Size = new System.Drawing.Size(191, 93);
+            this.btnMenuPrincipal.TabIndex = 2;
+            this.btnMenuPrincipal.Text = "Menu Principal";
+            this.btnMenuPrincipal.UseVisualStyleBackColor = true;
+            this.btnMenuPrincipal.Click += new System.EventHandler(this.btnMenuPrincipal_Click);
+            // 
+            // listBoxExtras
+            // 
+            this.listBoxExtras.FormattingEnabled = true;
+            this.listBoxExtras.ItemHeight = 25;
+            this.listBoxExtras.Location = new System.Drawing.Point(64, 515);
+            this.listBoxExtras.Name = "listBoxExtras";
+            this.listBoxExtras.Size = new System.Drawing.Size(774, 129);
+            this.listBoxExtras.TabIndex = 3;
             // 
             // FormReservas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1111, 768);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxExtras);
+            this.Controls.Add(this.btnMenuPrincipal);
             this.Controls.Add(this.listBoxMenus);
             this.Controls.Add(this.groupBox1);
             this.Name = "FormReservas";
@@ -141,6 +152,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnBuscarClientePorNif;
         private System.Windows.Forms.ListBox listBoxMenus;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMenuPrincipal;
+        private System.Windows.Forms.ListBox listBoxExtras;
     }
 }
